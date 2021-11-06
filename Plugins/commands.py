f2d3ecc8b9558a01240165cf35a0a1d2612de477
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-06 20:35:12 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 import logging
 import random
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -7,20 +8,3 @@ from info import ADMINS
 from translation import translation
 
 
-@Client.on_message(filters.command(["start"]) & filters.user(ADMINS))
-async def start(bot, update):
-    
-    buttons = [[
-        InlineKeyboardButton('Home ⚡', callback_data='start'),
-        InlineKeyboardButton('Close 🔐', callback_data='close')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
