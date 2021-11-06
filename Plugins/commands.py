20088a4e7c90d41733a35f60b3006ea3acca9c3a
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-06 20:38:04 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -8,3 +8,20 @@ from info import ADMINS
 from translation import translation
 
 
+#===================Start Function===================#
+@Client.on_message(filters.private & filters.command(['start']))
+async def start(client, message):
+    buttons = [[
+        InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk'),
+        InlineKeyboardButton('Update Channel ♻️', url='https://t.me/DX_Botz')
+        ],[
+        InlineKeyboardButton('💡 SouceCode', url='https://github.com/Jijinr/Frwdit-V2'),
+        InlineKeyboardButton('String Session 🎻', url ='https://replit.com/@JijinR/PyroSessionString?v=1')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await client.send_message(
+        chat_id=message.chat.id,
+        reply_markup=reply_markup,
+        text=Translation.START_TXT.format(
+                message.from_user.first_name),
+        parse_mode="html")
