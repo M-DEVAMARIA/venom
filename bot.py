6c2a2df0da5dc12fdbb2239806ac0c5bac874c12
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-06 19:48:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,12 +23,7 @@ class Bot(Client):
             sleep_threshold=5,
         )
 
-   async def start(self):
-        await super().start()
-        usr_bot_me = await self.get_me()
-        return (self, usr_bot_me.id)
-        print(f"with for Pyrogram v{__version__} (Layer {layer}) started on .")
-
+   
 
     async def stop(self, *args):
         await super().stop()
