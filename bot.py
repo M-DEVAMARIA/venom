feb78b8b824fc105bf497f68fa33128b2339f607
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-06 18:50:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 import logging
 import logging.config
 
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer 
