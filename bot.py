dcce545fbb4417c118aa61d65edf17bcbdfcc84b
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-06 19:36:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,13 +7,14 @@ logging.getLogger().setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer 
-from info import API_ID, API_HASH, BOT_TOKEN
+from info import API_ID, API_HASH, BOT_TOKEN, SESSION
 
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__( 
+            session_name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
