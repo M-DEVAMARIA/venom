b197622cc9f043cdb41fdaa4e15458567965d922
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-06 23:21:39 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -22,6 +22,16 @@ async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
         reply_markup=reply_markup,
-        text=Translation.START_TXT.format(
-                message.from_user.first_name),
+        text=Translation.START_TXT,
         parse_mode="html")
+
+ #==================about Function====================#
+@Client.on_message(filters.command('about'))
+async def bot_info(bot, message):
+    buttons = [
+        [
+            InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
+            InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+        ]
+        ]
+    await message.reply(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
