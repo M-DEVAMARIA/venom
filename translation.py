b6065594dd31d4b01bf686640d67c8c4b8386126
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-06 17:53:04 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,4 @@
+class Translation(object):
+    START_TEXT = """ <b> Hi \nI'm A Simple m.d File Renamer+File To Video Converter Bot With Permanent Thumbnail And Custom Caption support!</b> \n
+<b>Bot Maintained By: @mdadmin2 </b> \n 
+"""
