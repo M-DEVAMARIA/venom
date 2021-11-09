81577afeaae4c18166c383dc4fbec77277973741
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-10 00:08:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,14 +9,15 @@ API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 SESSION = environ.get('SESSION', 'Media_search')
 
-# Admins
+# Admins & channels
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://M_dautofilterv3:M_dautofilterv3@cluster0.wzriv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 DATABASE_NAME = environ.get('DATABASE_NAME', 'Md_movies')
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
-
+# bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
