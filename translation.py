d021ad2af7c15f53aa467da12875074a2ee8c3af
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-09 13:54:27 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,4 +1,4 @@
 class Translation(object):
-    START_TXT = """ <b> Hi \nI'm A Simple m.d File Renamer+File To Video Converter Bot With Permanent Thumbnail And Custom Caption support!</b> \n
+    START_TXT = """ <b> HEY {}\n IAM A FILTER BOT CURRENTLY MY ADMIN  BUILDING ME !</b> \n
 <b>Bot Maintained By: @mdadmin2 </b> \n 
 """
