b779ab5e724f8751535d8b8ac4f39cdde87865e8
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-09 14:30:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,3 +40,12 @@ async def bot_info(client, message):
         parse_mode="html")
     
  #==================restart Function====================#
+
+@Client.on_message(filters.private & filters.command(['restart']))
+async def restart(client, message):
+    msg = await message.reply_text(
+        text="<i>Trying to restarting.....</i>"
+    )
+    await asyncio.sleep(5)
+    await msg.edit("<i>Server restarted successfully ✅</i>")
+    os.execl(sys.executable, sys.executable, *sys.argv)
