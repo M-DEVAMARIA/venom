45c5a61f3d54e7cc07d26c146140e636d01fab3a
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-09 18:43:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,3 +12,10 @@ SESSION = environ.get('SESSION', 'Media_search')
 # Admins
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 
+# MongoDB information
+DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://erichdaniken:erichdaniken@cluster0.mggdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+DATABASE_NAME = environ.get('DATABASE_NAME', 'Md_movies')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+#bot setting
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
