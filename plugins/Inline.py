0bdd09d55391d17786a7a442d86b6aa13e9b3609
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-09 23:57:44 +0530

    Update Inline.py

--- plugins/Inline.py
+++ plugins/Inline.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 cache_time = 0 if ADMINS else CACHE_TIME
 
 
-@Client.on_inline_query(filters.user()
+@Client.on_inline_query()
 async def answer(bot, query):
     """Show search results for given inline query"""
 
