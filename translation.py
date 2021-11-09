6b310d6d60b3b8ab908bb2be8ad00e4f7f430333
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-09 14:13:48 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,4 +2,4 @@ class Translation(object):
     START_TXT = """ <b> HEY {}\n IAM A FILTER BOT CURRENTLY MY ADMIN  BUILDING ME !</b> \n
 <b>Bot Maintained By: @mdadmin2 </b> \n 
 """
-   ABOUT_TXT = """ CURRENTLY BUILDING ........"""
+   ABOUT_TXT = """ <b>CURRENTLY BUILDING """
