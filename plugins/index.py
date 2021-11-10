96df9a611dfce314d7291ef8368b455f31045c7e
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-10 18:47:19 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -23,11 +23,11 @@ async def index_files(bot, message):
         while True:
             lst_msg = await bot.ask(text = "Forward me last message of a channel which I should save to my database.\n\nYou can forward posts from any public channel, but for private channels bot should be an admin in the channel.\n\nMake sure to forward with quotes (Not as a copy)", chat_id = message.from_user.id)
             try:
-                lst_msg_id = lst_msg.forward_from_message_id
+                lst_msg_id = last_msg.forward_from_message_id
                 if last_msg.forward_from_chat.username:
-                    chat_id = lst_msg.forward_from_chat.username
+                    chat_id = last_msg.forward_from_chat.username
                 else:
-                    chat_id=lst_msg.forward_from_chat.id
+                    chat_id=last_msg.forward_from_chat.id
                 await bot.get_messages(chat_id, lst_msg_id)
                 break
             except Exception as e:
