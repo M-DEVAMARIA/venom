39e24777927e9efe72433c301d54911d8d5d3c59
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-10 16:55:36 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -29,7 +29,10 @@ async def index_files(bot, message):
             except Exception as e:
                 await last_msg.reply_text(f"This Is An Invalid Message, Either the channel is private and bot is not an admin in the forwarded chat, or you forwarded message as copy.\nError caused Due to <code>{e}</code>")
                 continue
-                
+  msg = query.message
+
+    await query.answer('Processing...⏳', show_alert=True)
+
     await msg.edit(
         "Starting Indexing",
         reply_markup=InlineKeyboardMarkup(
