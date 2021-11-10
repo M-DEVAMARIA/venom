1ace5c2eec50b07791f98b99e100f82b92330b3b
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-10 22:51:54 +0530

    Update users_db.py

--- database/users_db.py
+++ database/users_db.py
@@ -1,1 +1,19 @@
+import motor.motor_asyncio
+from info import DATABASE_NAME, DATABASE_URI
 
+class Database:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+        self.grp = self.db.groups
+
+#_________total users _______________#
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+      
+#_________all users___________________#
+    async def get_all_users(self):
+        return self.col.find({})
