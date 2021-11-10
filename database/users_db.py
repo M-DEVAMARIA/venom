8d64507910418e44fa507d9fc256af518b1cc3a9
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-10 23:28:46 +0530

    Update users_db.py

--- database/users_db.py
+++ database/users_db.py
@@ -8,11 +8,23 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
+        
+#______________new user its imprtant______________#
+        def new_user(self, id, name):
+        return dict(
+            id = id,
+            name = name,
+            ban_status=dict(
+                is_banned=False,
+                ban_reason="",
+            ),
+        )
 
 #_________total users _______________#
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
+   
       
 #_________all users___________________#
     async def get_all_users(self):
