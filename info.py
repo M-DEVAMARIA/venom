7bc5ea236f46429bb88282b0ce1ae5c792d50ccf
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-10 22:24:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,3 +28,6 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+
+#broadcast db
+db = Database(DATABASE_URI, DATABASE_NAME)
