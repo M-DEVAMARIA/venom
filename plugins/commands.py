7be6d7249584fe1ddeec53b1254159a7a6f7db61
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-11 19:32:07 +0530

    Revert "Update commands.py"
    This reverts commit 1e24f588d504a0026d988db9990e50bb5257daa9.

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,7 +30,7 @@ async def start(client, message):
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await message.send_photo(
+    await client.send_photo(
         photo=random.choice(PHOTO),
         chat_id=message.chat.id,
         reply_markup=reply_markup,
