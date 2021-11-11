323e261a94535f3a228a6efc817e03bdc47884bb
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-11 19:32:07 +0530

    Revert "Delete users_db.py"
    This reverts commit d920e8ab2087c8cda7786132a54d02ed3980176e.

---
+++ database/users_db.py
@@ -0,0 +1,75 @@
+import motor.motor_asyncio
+from info import DATABASE_NAME, DATABASE_URI
+
+class Database:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+        self.grp = self.db.groups
+        
+#_______new user its imprtant________#
+    def new_user(self, id, name):
+        return dict(
+            id = id,
+            name = name,
+            ban_status=dict(
+                is_banned=False,
+                ban_reason="",
+            ),
+        )
+
+#_________total users _______________#
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+   
+      
+#_________all users___________________#
+    async def get_all_users(self):
+        return self.col.find({})
+    
+    
+#________(account) delete user_________#
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+
+    
+   
+
+#____bannning code test for broadcast___#
+    async def remove_ban(self, id):
+        ban_status = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
+        )
+        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
+
+    async def ban_user(self, user_id, ban_duration, ban_reason):
+        ban_status = dict(
+            is_banned=True,
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason,
+        )
+        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
+
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
+        )
+        user = await self.col.find_one({"id": int(id)})
+        return user.get("ban_status", default)
+     
+    async def get_all_banned_users(self):
+        banned_users = self.col.find({"ban_status.is_banned": True})
+        return banned_users
+
+db=Database(DATABASE_URI, DATABASE_NAME)
