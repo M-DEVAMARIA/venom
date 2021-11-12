f9e7c71bfb2e5d8af1948e8c0789f9fa7343f738
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-12 14:18:24 +0530

    Update Pm_filter.py

--- plugins/Pm_filter.py
+++ plugins/Pm_filter.py
@@ -7,7 +7,7 @@ from pyrogram.errors import UserNotParticipant
 from utils import get_filter_results, get_file_details, is_subscribed, get_poster
 BUTTONS = {}
 BOT = {}
-@Client.on_message(filters.private & filters.user(AUTH_USERS) if AUTH_USERS else filters.users & filters.private)
+@Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming & filters.user)
 async def filter(client, message):
     if message.text.startswith("/"):
         return
