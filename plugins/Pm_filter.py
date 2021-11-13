e03be406a3c6ee6764ba18c4ca21fc04b0027559
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-14 00:09:29 +0530

    Update Pm_filter.py

--- plugins/Pm_filter.py
+++ plugins/Pm_filter.py
@@ -397,6 +397,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.edit_text(
             text="<b> hey user my name is venom i will send movies </b>\n",
             reply_markup=reply_markup,
-            parse_mode='html'
+            parse_mode='markdown'
 
  
