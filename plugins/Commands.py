2ef7143d4da5f794071efeca3501c3afbe3272ce
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-13 17:53:28 +0530

    Update Commands.py

--- plugins/Commands.py
+++ plugins/Commands.py
@@ -177,14 +177,14 @@ async def stats(client, message):
 
  time = def time_formatter(seconds: float) -> str:
         """ 
-    humanize time 
-    """
-    minutes, seconds = divmod(int(seconds),60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    tmp = ((str(days) + "d, ") if days else "") + \
-        ((str(hours) + "h, ") if hours else "") + \
-        ((str(minutes) + "m, ") if minutes else "") + \
-        ((str(seconds) + "s") if seconds else "")
-    return tmp
+        humanize time 
+        """
+          minutes, seconds = divmod(int(seconds),60)
+          hours, minutes = divmod(minutes, 60)
+          days, hours = divmod(hours, 24)
+          tmp = ((str(days) + "d, ") if days else "") + \
+             ((str(hours) + "h, ") if hours else "") + \
+             ((str(minutes) + "m, ") if minutes else "") + \
+             ((str(seconds) + "s") if seconds else "")
+         return tmp
 
