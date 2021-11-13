fc2517c294371c597183913302433b6f5e05b0b1
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-14 00:28:26 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,4 +6,9 @@ class Translation(object):
 🕐Bot's Uptime: <code>{}</code> \n
 """
 
-
+    AUTOFILTER_TXT = """ <b>Auto Filter</b>
+<b>NOTE:</b>
+1. Make me the admin of your channel if it's private.
+2. make sure that your channel does not contains cam rip, porn and fake files.
+3. Forward the last message to me with quotes.
+ I'll add all the files in that channel to my db."""
