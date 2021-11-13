38fe57ec5a809f7dc496c6cad9fb9bb88851f3d8
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-13 19:08:21 +0530

    Update Pm_filter.py

--- plugins/Pm_filter.py
+++ plugins/Pm_filter.py
@@ -1,5 +1,5 @@
 #Kanged From @TroJanZheX
-from info import AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, API_KEY, AUTH_GROUPS, BUTTON
+from info import AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, API_KEY, AUTH_GROUPS, BUTTON, start_uptime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
 import re, time
