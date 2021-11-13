cbee36d456ce30df2465c0add608b0e848f8f9a5
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-13 14:20:34 +0530

    Update Commands.py

--- plugins/Commands.py
+++ plugins/Commands.py
@@ -132,7 +132,7 @@ async def start(bot, cmd):
 @Client.on_message(filters.command(['about']))
 async def bot_info(client, message):
     buttons = [[
-            InlineKeyboardButton('Update Channel', url="about"),
+            InlineKeyboardButton('Update Channel', callback_data="about"),
             InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
