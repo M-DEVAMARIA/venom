16ba147d9638fd3591c30fb42d5905e3907fe539
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-14 20:47:55 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,13 +2,13 @@ class Translation(object):
     START_TXT = """ <b> HEY {}\n IAM A FILTER BOT CURRENTLY MY ADMIN  BUILDING ME !</b> \n
 <b>Bot Maintained By: @mdadmin2 </b> \n 
 """
-    ABOUT_TXT = """ uptime : <code>{}</code>
-MY NAME : <a href="t.me/md_moviesees">VENOM ROBOT</a>\n
-👦CREATOR : <a href="t.me/MDADMIN2>MDADMIN2</a>\n
-📡Hosted On: <a href="https://t.me/MD_MOVIESES">Railway</a>\n
-🗣️LANGUAGE : <a href="https://docs.pyrogram.org">Python3</a>\n
-📚LIBRARY : <a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a>\n
- 🗒️Version : <a href="t.me/mdad2min">1.0.0(beta)</a>\n
+    ABOUT_TXT = """ <b>uptime : <code>{}</code> </>b
+MY NAME : <a href="t.me/md_moviesees">VENOM ROBOT</a>
+👦CREATOR : <a href="t.me/MDADMIN2>MDADMIN2</a>
+📡Hosted On: <a href="https://t.me/MD_MOVIESES">Railway</a>
+🗣️LANGUAGE : <a href="https://docs.pyrogram.org">Python3</a>
+📚LIBRARY : <a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a>
+🗒️Version : <a href="t.me/mdad2min">1.0.0(beta)</a>
 """
 
     AUTOFILTER_TXT = """ <b>Auto Filter</b>
