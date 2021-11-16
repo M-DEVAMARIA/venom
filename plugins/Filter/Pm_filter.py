294bf5690a3fc29e82e8acaaa42370cc624385f8
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-16 23:47:07 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -8,7 +8,7 @@ from translation import Translation
 from pyrogram.errors import UserNotParticipant
 from utils import Media, get_filter_results, get_file_details, is_subscribed, get_poster, time_formatter
 from database.users_db import db
-from .inline import RATING, GENRES
+from .Inline import RATING, GENRES
 import random
 BUTTONS = {}
 BOT = {}
