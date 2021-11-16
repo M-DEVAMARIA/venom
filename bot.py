44403f5e8d7102102be06c28557cbf1679e010bf
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-16 17:33:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer 
 from utils import Media
 from info import API_ID, API_HASH, BOT_TOKEN, SESSION 
-import pyromod.listen
+
 
 class Bot(Client):
 
