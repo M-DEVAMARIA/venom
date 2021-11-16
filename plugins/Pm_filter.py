8724ed62ec46ebd93d1e6a40357988c9b01e2306
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-16 19:11:55 +0530

    Update Pm_filter.py

--- plugins/Pm_filter.py
+++ plugins/Pm_filter.py
@@ -370,7 +370,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
  
     if query.data == "close":
         await query.message.delete()
-t
+
 
 
         
@@ -477,18 +477,4 @@ t
             parse_mode='html'
       )
         
-    elif query.data == "pages":
-       await query.answer()
-    elif query.data == "close":
-          try:
-            await query.message.reply_to_message.delete()
-            await query.message.delete()
-          except:
-            await query.message.delete()
-                
-    else:
-        await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ👀",show_alert=True)
-        
- 
-    if query.data == "close":
-        await query.message.delete()
+    
