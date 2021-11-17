75dbeb0e75f85c4ca42a155a2a9c460986683c99
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-17 19:56:46 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -79,7 +79,7 @@ So you go to google and check the spelling of the name of the movie you want.
             reply_markup=InlineKeyboardMarkup(
                       [[ 
 
-                         InlineKeyboardButton("🔍 GOOGLE 🔎", url=f'https://www.google.com/)
+                         InlineKeyboardButton("🔍 GOOGLE 🔎", url=f'https://www.google.com/')
                         ]]
                 ),     
             parse_mode="html",
