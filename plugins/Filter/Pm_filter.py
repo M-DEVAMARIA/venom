e1e7f17c28e224b179778dde7a0b4336a12e85a2
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-17 18:41:24 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -55,7 +55,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        leng = FIND.get(query).get("total_len")
+        leng = get("total_len")
         imdb = f"**🗂️ Title:** {search}\n🗃️ Total Files : {leng}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded by {message.chat.title}**" 
         files = await get_filter_results(query=search)
         if files:
