ef13de2a970c5962280073d8668f10927ffadc58
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-18 15:35:57 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TXT = """ <b> HEY {}\n IAM A FILTER BOT CURRENTLY MY ADMIN  BUILDING ME !</b> \n
+    START_TXT = """ <b> HEY {}\n my name is venom i will give movies in your group and pm !</b> \n
 <b>Bot Maintained By: @mdadmin2 </b> \n 
 """
     ABOUT_TXT = """ <b>uptime :{}</b>\n📃Bot : <a href="t.me/md_movises">venom Robot</a>\n👦Creator : <a href="t.me/mdadmin2">MDADMIN</a>\n📡Hosted On : <a href="https://t.me/md_movises">Railway</a>\n🗣️Language : <a href="https://docs.pyrogram.org">Python3</a>\n📚Library : <a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a>\n🗒️Version : <a href="t.me/md_movises">2.9.0</a>"""
@@ -8,7 +8,8 @@ class Translation(object):
 
     AUTOFILTER_TXT = """ <b>Auto Filter</b>
 <b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
+
+1. Make me the admim of your channel if it's private .
 2. make sure that your channel does not contains cam rip, porn and fake files.
 3. Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
