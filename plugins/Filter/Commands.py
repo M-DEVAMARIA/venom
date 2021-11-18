1503fe417f7196353d665f4a0c24f03999be06cf
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-18 14:34:25 +0530

    Update Commands.py

--- plugins/Filter/Commands.py
+++ plugins/Filter/Commands.py
@@ -176,16 +176,16 @@ async def stats(client, message):
 
  #_______________logs__________________#
 
-@Client.on_message(filters.command('logs') & filters.user(ADMINS))
-
+@Client.on_message(filters.command('logger') & filters.user(ADMINS))
 async def log_file(bot, message):
-
     """Send log file"""
-
     try:
-
         await message.reply_document('TelegramBot.log')
-
     except Exception as e:
-
         await message.reply(str(e))
+    
+
+        
+
+
+       
