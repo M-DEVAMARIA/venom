8a98bb1c294a86b1f09112cc8c42c802db1a7102
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-18 14:49:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS
 DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://M_dautofilterv3:M_dautofilterv3@cluster0.wzriv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 DATABASE_NAME = environ.get('DATABASE_NAME', 'Md_movies')
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-
+DATABASE_NAME2 = environ.get('DATABASE_NAME2', 'users')
 # bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
