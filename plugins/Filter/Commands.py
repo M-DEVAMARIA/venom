63ac32d5b77e791f14bd0f5293381a53b5da0848
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-19 00:58:38 +0530

    Update Commands.py

--- plugins/Filter/Commands.py
+++ plugins/Filter/Commands.py
@@ -125,15 +125,14 @@ async def start(bot, cmd):
             )
         )
     else:
-        me = await self.get_me()
-        self.username = '@' + me.username
+        
         await cmd.reply_photo(
         photo=random.choice(PHOTO), 
         caption=Translation.START_TXT.format(cmd.from_user.first_name),
         parse_mode="html",
             reply_markup=InlineKeyboardMarkup(
                 [[
-                        InlineKeyboardButton('➕ ADD ME TO YOUR GROUP ➕', url=f'http://t.me/{me.username}?startgroup=true'),
+                        InlineKeyboardButton('➕ ADD ME TO YOUR GROUP ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true'),
                         ],[
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
                         InlineKeyboardButton("🤖 VENOM UPDATES", url=f"https://t.me/joinchat/EOI9s4lc00cyOTI1")
