f6621f67924b11e60bfc033a94d8bff74b6ec05b
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-22 23:24:26 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -12,6 +12,7 @@ from .Inline import RATING, GENRES
 import random
 BUTTONS = {}
 BOT = {}
+SPELL_CHECK = {}
 @Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
 async def filter(client, message):
     if message.text.startswith("/"):
