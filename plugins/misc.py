062fa4d34d957718da97c7abd9c6740af89f7076
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-22 19:48:01 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,1 +1,13 @@
+import os
+from pyrogram import filters, Client
+from translation import Translation
 
+
+@Client.on_message(filters.command(['info']))
+async def bot_info(client, message):
+    
+    await client.send_message(
+        chat_id=message.chat.id,
+       
+        text=Translation.ABOUT_TXT,
+        parse_mode="html")
