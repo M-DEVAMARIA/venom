36a7f14a2f07a80f8949919851911aa17311e693
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-23 11:33:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -367,4 +367,17 @@ def time_formatter(seconds: float) -> str:
         ((str(minutes) + "m, ") if minutes else "") + \
         ((str(seconds) + "s") if seconds else "")
     return tmp
+#_________music ___________#
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
 
