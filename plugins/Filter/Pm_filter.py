f08f59bc9a7a3857d0ddce8806bb12dcf9f8e2e5
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-24 02:08:06 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -111,7 +111,7 @@ So you go to google and check the spelling of the name of the movie you want.
             poster=None
             
             imdb = await get_poster(search, file=(files[0]).file_name)
-            if poster
+            if poster:
                 await message.reply_photo(photo=imdb.get('poster'), caption=imdb, reply_markup=InlineKeyboardMarkup(buttons))
                
 
