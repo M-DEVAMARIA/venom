70f68d5e949e36faff065e8b045782fc1abd7370
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-23 19:32:50 +0530

    Update Song.py

--- plugins/Broadcast/Song.py
+++ plugins/Broadcast/Song.py
@@ -96,5 +96,4 @@ def a(client, message):
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
-app = Bot()
-app.run()
+
