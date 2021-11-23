bd5fb8eec5e3b776b2805abb10c16d2f1cefe38a
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-23 15:14:26 +0530

    Update Song_ut.py

--- Song_ut.py
+++ Song_ut.py
@@ -1,1 +1,12 @@
+from Python_ARQ import ARQ
+from pyrogram import Client
+import aiofiles
+import aiohttp
+import ffmpeg
+import requests
+import wget
 
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+aiohttpsession = aiohttp.ClientSession()
+chat_id = None
+arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
