15614d369461802f6d288a94f0a48a24e914fe04
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-24 14:39:17 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -126,7 +126,7 @@ So you go to google and check the spelling of the name of the movie you want.
             poster=None
             imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
             if imdb:
-            cap = IMDB_TEMPLATE.format(query = search,
+                cap = IMDB_TEMPLATE.format(query = search,
                                    rating = imdb['rating'],
                                    title = imdb['title'],
                                    year = imdb['year'],
