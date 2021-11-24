f8b79685a7525f0da89dd3c2c7f34bda4074dbfb
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-24 14:13:46 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -124,9 +124,20 @@ So you go to google and check the spelling of the name of the movie you want.
             if BUTTON:
                 buttons.append([InlineKeyboardButton(text="Close ❌",callback_data="close")])
             poster=None
+                imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
+    if imdb:
+        cap = IMDB_TEMPLATE.format(query = search,
+                                   rating = imdb['rating'],
+                                   title = imdb['title'],
+                                   year = imdb['year'],
+                                   genres = imdb['genres'],
+                                   poster = imdb['poster'],
+                                   **locals()
+                                )
+    else:
+        cap = f"sorry no imdb found"
             
-            poster = await get_poster(search, file=(files[0]).file_name)
-            if poster:
+            if imdb and imdb.get('poster'):
                 await message.reply_photo(photo=poster.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(buttons))
                
 
