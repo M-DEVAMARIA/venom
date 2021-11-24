f6d612d29fd907423b95e5fbe86bc00d9173c987
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-24 11:58:23 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -61,7 +61,11 @@ async def filter(client, message):
         search = message.text
         leng = ("total_len")
         google = "https://telegra.ph/file/5c6a4fea12bd4a42d690d.mp4"
-        cap = IMDB_TEMPLATE.format(query = search, rating = imdb['rating'], title = imdb['title'], year = imdb['year'], genres = imdb['genres'],  poster = imdb['poster'])
+        cap = IMDB_TEMPLATE.format(query = search, rating = imdb['rating'],
+                                   title = imdb['title'],
+                                   year = imdb['year'],
+                                   genres = imdb['genres'],
+                                   poster = imdb['poster'])
         imdb = f"**🗂️ Title:** {search}\n🗃️ Total Files : {leng}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded by {message.chat.title}**" 
         files = await get_filter_results(query=search)
         if files:
