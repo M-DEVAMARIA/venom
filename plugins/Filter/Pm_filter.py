8c3a1515bde61742ed28b0df120b09b821ddd7ea
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-25 00:50:00 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -197,7 +197,7 @@ async def group(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"**↪️ Requested:** {query}\n\n🎞️ Title: {search}\n🎭 Genres: {poster.get('genres')}\n📆 Year: {poster.get('year')}\n🌟Rating: <a href={poster['url']}/ratings>{poster.get('rating')}</a>/10\n📑 Total Page : 1/1\n🗃️ TotalFiles : None\n👤requested by: {message.from_user.mention}", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster.get('poster'), caption=f"**↪️ Requested:** {query}\n\n🎞️ Title: {search}\n🎭 Genres: {poster.get('genres')}\n📆 Year: {poster.get('year')}\n🌟Rating: <a href={poster['url']}/ratings>{poster.get('rating')}</a>/10\n📑 Total Page : 1/1\n🗃️ TotalFiles : None\n👤requested by: {message.from_user.mention}", reply_markup=InlineKeyboardMarkup(buttons))
             else:
                 await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
@@ -217,7 +217,7 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"**↪️ Requested:** {query}\n\n🎞️ Title: {search}\n🎭 Genres: {poster.get('genres')}\n📆 Year: {poster.get('year')}\n🌟Rating: <a href={poster['url']}/ratings>{poster.get('rating')}</a> / 10\n📑 Total Page : <code>1 / {data['total']}</code>\n🗃️ TotalFiles : None\n👤requested by: {message.from_user.mention}", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster.get('poster'), caption=f"**↪️ Requested:** {query}\n\n🎞️ Title: {search}\n🎭 Genres: {poster.get('genres')}\n📆 Year: {poster.get('year')}\n🌟Rating: <a href={poster['url']}/ratings>{poster.get('rating')}</a> / 10\n📑 Total Page : <code>1 / {data['total']}</code>\n🗃️ TotalFiles : None\n👤requested by: {message.from_user.mention}", reply_markup=InlineKeyboardMarkup(buttons))
         else:
             await message.reply_text(caption=imdb, reply_markup=InlineKeyboardMarkup(buttons))
 
