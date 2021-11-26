f532b6abefb624f560b3ee20ca739daf1a08309c
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-27 00:09:52 +0530

    Update Commands.py

--- plugins/Filter/Commands.py
+++ plugins/Filter/Commands.py
@@ -207,7 +207,29 @@ async def log_file(bot, message):
         await message.reply(str(e))
     
 
-        
+@Client.on_message((filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
+async def report(bot, message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        reporter = str(message.from_user.id)
+        mention = message.from_user.mention
+        admins = await bot.get_chat_members(chat_id=chat_id, filter="administrators")
+        success = False
+        report = f"Reporter : {mention} ({reporter})" + "\n"
+        report += f"Message : {message.reply_to_message.link}"
+        for admin in admins:
+            try:
+                reported_post = await message.reply_to_message.forward(admin.user.id)
+                await reported_post.reply_text(
+                    text=report,
+                    chat_id=admin.user.id,
+                    disable_web_page_preview=True
+                )
+                success = True
+            except:
+                pass
+        if success:
+            await message.reply_text("**Reported to Admins!**")
 
 
        
