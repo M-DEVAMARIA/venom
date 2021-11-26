49b5cfab5d3b03f9471aac056c439a28596f5a61
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-26 14:36:00 +0530

    Update callback.py

--- plugins/Broadcast/callback.py
+++ plugins/Broadcast/callback.py
@@ -1,1 +1,25 @@
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from translation import Translation
 
+
+
+
+
+
+
+
+Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+  
+  
+    elif query.data == "song":
+        buttons = [[
+            InlineKeyboardButton('👩‍🦯 Back', callback_data='help')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=Translation.AUTOFILTER_TXT,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
