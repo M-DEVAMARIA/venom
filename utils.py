25ce2a68e7331e9dc21f9bfafa1352bf4c178438
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-26 19:31:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -462,3 +462,19 @@ def parser(text, keyword):
     except:
         return note_data, buttons, None
 
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
