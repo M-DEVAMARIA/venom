e533e88c45e00e85a101c2748db884c7b9216bd8
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-27 23:15:10 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -6,6 +6,7 @@ import re, time, asyncio
 import re
 import ast
 import pyrogram
+from plugins.Broadcast.callback import give_filter
 from translation import Translation
 from pyrogram.errors import UserNotParticipant
 from utils import Media, get_filter_results, get_file_details, is_subscribed, get_poster, time_formatter, temp, search_gagala
