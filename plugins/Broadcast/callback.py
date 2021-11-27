5f278ce80603bf9a180991341a6fd287233ad1a8
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-27 14:36:58 +0530

    Create callback.py

---
+++ plugins/Broadcast/callback.py
@@ -0,0 +1,46 @@
+import os
+from pyrogram import Client, filters
+from database.filters_mdb import(
+   del_all,
+   find_filter,
+   get_filters,
+)
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+import pyrogram
+
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    if query.data == "close_data":
+        await query.message.delete()
+    elif query.data == "delallconfirm":
+        userid = query.from_user.id
+        chat_type = query.message.chat.type
+
+        if chat_type == "private":
+            grpid  = await active_connection(str(userid))
+            if grpid is not None:
+                grp_id = grpid
+                try:
+                    chat = await client.get_chat(grpid)
+                    title = chat.title
+                except:
+                    await query.message.edit_text("Make sure I'm present in your group!!", quote=True)
+                    return
+            else:
+                await query.message.edit_text(
+                    "I'm not connected to any groups!\nCheck /connections or connect to any groups",
+                    quote=True
+                )
+                return
+
+        elif chat_type in ["group", "supergroup"]:
+            grp_id = query.message.chat.id
+            title = query.message.chat.title
+
+        else:
+            return
+        st = await client.get_chat_member(grp_id, userid)
+        if (st.status == "creator") or (str(userid) in ADMINS):    
+            await del_all(query.message, grp_id, title)
+        else:
+            await query.answer("You need to be Group Owner or an Auth User to do that!",show_alert=True)
