079232904538c82b4ab8bdeae834fad368e9e15c
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-29 16:02:16 +0530

    Update custom.py

--- plugins/Broadcast/custom.py
+++ plugins/Broadcast/custom.py
@@ -99,13 +99,13 @@ Made by @FayasNoushad"""
         return error
 
 #-------------------pin---------------------#
-@Client.on_message(filters.command(["pin"])
+@Client.on_message(filters.command("pin"))
 async def pin(_, message: Message):
     if not message.reply_to_message:
         return
     await message.reply_to_message.pin()
 
-@Client.on_message(filters.command(["unpin"])
+@Client.on_message(filters.command("unpin"))
 async def unpin(_, message: Message):
     if not message.reply_to_message:
         return
