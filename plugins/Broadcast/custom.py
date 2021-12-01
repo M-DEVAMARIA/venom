574a3585b1f2c872ea6338b489fccf28ba7f0b42
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-01 11:49:03 +0530

    Update custom.py

--- plugins/Broadcast/custom.py
+++ plugins/Broadcast/custom.py
@@ -112,43 +112,4 @@ async def unpin(_, message: Message):
     await message.reply_to_message.unpin()
     
 
-#_________________calculator_________________#
-from plugins.Filter.Pm_filter import cb_data
-CALCULATE_TEXT = "search in {temp.U_NAME}"
-CALCULATE_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton("DEL", callback_data="DEL"),
-        InlineKeyboardButton("AC", callback_data="AC"),
-        InlineKeyboardButton("(", callback_data="("),
-        InlineKeyboardButton(")", callback_data=")")
-        ],[
-        InlineKeyboardButton("7", callback_data="7"),
-        InlineKeyboardButton("8", callback_data="8"),
-        InlineKeyboardButton("9", callback_data="9"),
-        InlineKeyboardButton("÷", callback_data="/")
-        ],[
-        InlineKeyboardButton("4", callback_data="4"),
-        InlineKeyboardButton("5", callback_data="5"),
-        InlineKeyboardButton("6", callback_data="6"),
-        InlineKeyboardButton("×", callback_data="*")
-        ],[
-        InlineKeyboardButton("1", callback_data="1"),
-        InlineKeyboardButton("2", callback_data="2"),
-        InlineKeyboardButton("3", callback_data="3"),
-        InlineKeyboardButton("-", callback_data="-"),
-        ],[
-        InlineKeyboardButton(".", callback_data="."),
-        InlineKeyboardButton("0", callback_data="0"),
-        InlineKeyboardButton("=", callback_data="="),
-        InlineKeyboardButton("+", callback_data="+"),
-        ]]
-    )
-@Client.on_message(filters.private & filters.command(["calc", "calculate", "calculator"]))
-async def calculate(bot, update):
-    await update.reply_text(
-        text=CALCULATE_TEXT,
-        reply_markup=CALCULATE_BUTTONS,
-        disable_web_page_preview=True,
-        quote=True
-    )
 
