30d1c4a700f8fbcf46b3aa5857fad06e21bc3d49
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-01 14:10:08 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -128,7 +128,7 @@ So you go to google and check the spelling of the name of the movie you want.
             poster=await get_poster(search)
         if poster:
             
-                await message.reply_photo(photo=poster.get('poster'), caption=f"**↪️ Requested:** {query}\n\n**🎬 Title:** {poster.get('title')}\n**🎭 Genres:** {poster.get('genres')}\n**📆 Year:** <a href={poster['url']}/releaseinfo>{poster.get('year')}</a>\n**🌟Rating:** <a href={poster['url']}/ratings>{poster.get('rating')}</a> / 10\n**📑 Total Page :** <code>1 to 1</code>\n**🗃️ TotalFiles :** None\n**👤requested by:** {message.from_user.mention}", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster.get('poster'), caption=f"**↪️ Requested:** {query}\n\n**🎬 Title:** {poster.get('title')}\n**🎭 Genres:** {poster('genres')}\n**📆 Year:** <a href={poster['url']}/releaseinfo>{poster.get('year')}</a>\n**🌟Rating:** <a href={poster['url']}/ratings>{poster.get('rating')}</a> / 10\n**📑 Total Page :** <code>1 to 1</code>\n**🗃️ TotalFiles :** None\n**👤requested by:** {message.from_user.mention}", reply_markup=InlineKeyboardMarkup(buttons))
 
         else:
                 await message.reply_text(imdb, reply_markup=InlineKeyboardMarkup(buttons))
