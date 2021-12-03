9ae8e030bbc26d13fc65cac4f7f825c6f2b95804
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-03 17:25:56 +0530

    Update custom.py

--- plugins/Broadcast/custom.py
+++ plugins/Broadcast/custom.py
@@ -111,5 +111,21 @@ async def unpin(_, message: Message):
         return
     await message.reply_to_message.unpin()
     
+import wikipedia
 
+
+
+
+@Client.on_message(pattern="wiki ?(/)")
+async def wiki(e):
+    srch = e.pattern_match.group(1)
+    if not srch:
+        return await eor(e, "`Give some text to search on wikipedia !`")
+    msg = await eor(e, f"`Searching {srch} on wikipedia..`")
+    try:
+        mk = wikipedia.summary(srch)
+        te = f"**Search Query :** {srch}\n\n**Results :** {mk}"
+        await msg.edit(te)
+    except Exception as e:
+        await msg.edit(f"**ERROR** : {str(e)}")
 
