69489d7d89635a0c3a61454c6f5b13f09bcda98a
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-05 00:52:45 +0530

    Update Store.py

--- plugins/Filter/Store.py
+++ plugins/Filter/Store.py
@@ -58,3 +58,13 @@ async def batch(c, m):
     url = f"https://t.me/{bot.username}?start={base64_string}"
 
     await message.edit(text=url)
+    
+    
+async def encode_string(string):
+    string_bytes = string.encode("ascii")
+    base64_bytes = base64.b64encode(string_bytes)
+    base64_string = base64_bytes.decode("ascii")
+    return base64_string
+
+
+
