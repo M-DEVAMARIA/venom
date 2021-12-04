dfe60725beed7764fea03dc25195aa392ef33e3b
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-05 00:39:29 +0530

    Create Store.py

---
+++ plugins/Filter/Store.py
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+@Client.on_message(filters.command('batch') & filters.private & filters.incoming)
+async def batch(c, m):
+    """ This is for batch command"""
+    if IS_PRIVATE:
+        if m.from_user.id not in AUTH_USERS:
+            return
+    BATCH.append(m.from_user.id)
+    files = []
+    i = 1
+
+    while m.from_user.id in BATCH:
+        if i == 1:
+            media = await c.ask(chat_id=m.from_user.id, text='Send me some files or videos or photos or text or audio. If you want to cancel the process send /cancel')
+            if media.text == "/cancel":
+                return await m.reply_text('Cancelled Successfully ✌')
+            files.append(media)
+        else:
+            try:
+                reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Done ✅', callback_data='done')]])
+                media = await c.ask(chat_id=m.from_user.id, text='Ok 😉. Now send me some more files Or press done to get shareable link. If you want to cancel the process send /cancel', reply_markup=reply_markup)
+                if media.text == "/cancel":
+                    return await m.reply_text('Cancelled Successfully ✌')
+                files.append(media)
+            except ListenerCanceled:
+                pass
+            except Exception as e:
+                print(e)
+                await m.reply_text(text="Something went wrong. Try again later.")
+        i += 1
+
+    message = await m.reply_text("Generating shareable link 🔗")
+    string = ""
+    for file in files:
+        if DB_CHANNEL_ID:
+            copy_message = await file.copy(int(DB_CHANNEL_ID))
+        else:
+            copy_message = await file.copy(m.from_user.id)
+        string += f"{copy_message.message_id}-"
+        await asyncio.sleep(1)
+
+    string_base64 = await encode_string(string[:-1])
+    send = await c.send_message(m.from_user.id, string_base64) if not DB_CHANNEL_ID else await c.send_message(int(DB_CHANNEL_ID), string_base64)
+    base64_string = await encode_string(f"batch_{m.chat.id}_{send.message_id}")
+    bot = await c.get_me()
+    url = f"https://t.me/{bot.username}?start={base64_string}"
+
+    await message.edit(text=url)
