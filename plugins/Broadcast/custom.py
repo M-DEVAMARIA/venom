1197b3175ee981ed1c6f177d154a7d20b03f33f9
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-04 14:56:03 +0530

    Update custom.py

--- plugins/Broadcast/custom.py
+++ plugins/Broadcast/custom.py
@@ -1,4 +1,5 @@
 import os
+import time
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from telegraph import upload_file
@@ -113,7 +114,14 @@ async def unpin(_, message: Message):
     
 import wikipedia
 
-
+#_____________________ping_____________________
+@Client.on_message(filters.command("ping"))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
 
 @Client.on_message(filters.reply & filters.command("wiki"))
