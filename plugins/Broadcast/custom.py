27067e3ebb1f335106a1fe5cb1df92af4d684363
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-08 23:37:12 +0530

    Update custom.py

--- plugins/Broadcast/custom.py
+++ plugins/Broadcast/custom.py
@@ -123,7 +123,7 @@ async def ping(_, message):
     time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
-
+#==========================================================
 @Client.on_message(filters.reply & filters.command("wiki"))
 async def wiki(bot, message):
     srch = message.reply_to_message
@@ -138,3 +138,19 @@ async def wiki(bot, message):
         await msg.edit(f"**ERROR** : {str(e)}")
     b_msg = message.reply_to_message
 
+#=============================================================
+from io import BytesIO
+
+@Client.on_message(filters.command(["json", "response"]), group=1)
+async def response_json(bot, update):
+    json = update.reply_to_message
+    with BytesIO(str.encode(str(json))) as json_file:
+        json_file.name = "JSON.text"
+        await json.reply_document(
+            document=json_file,
+            quote=True
+        )
+        try:
+            os.remove(json_file)
+        except:
+            pass
