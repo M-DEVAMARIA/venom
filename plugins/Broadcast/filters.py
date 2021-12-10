3f06b8c255a359fa0836474655e1cbdf44d6047c
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-10 19:30:30 +0530

    Update filters.py

--- plugins/Broadcast/filters.py
+++ plugins/Broadcast/filters.py
@@ -395,7 +395,7 @@ async def connections(client,message):
             ttl = await client.get_chat(int(groupid))
             title = ttl.title
             active = await if_active(str(userid), str(groupid))
-            act = " - ACTIVE" if active else ""
+            act = "✔️" if active else ""
             buttons.append(
                 [
                     InlineKeyboardButton(
