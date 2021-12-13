9984896fc90a6a11d44c3bd8a6841faeb25e6e70
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-13 17:00:58 +0530

    Update settings.py

--- plugins/Broadcast/settings.py
+++ plugins/Broadcast/settings.py
@@ -3,4 +3,14 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.errors import ButtonDataInvalid, FloodWait
 
-
+@Client.on_message(filters.command(['settinga'])& filters.group, group=1)
+async def bot_info(client, message):
+    buttons = [[
+            InlineKeyboardButton("🤖 Venom UPDATES", url=f"https://t.me/joinchat/EOI9s4lc00cyOTI1")
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await client.send_message(
+        chat_id=message.chat.id,
+        reply_markup=reply_markup,
+        text=Translation.ABOUT_TXT,
+        parse_mode="html")
