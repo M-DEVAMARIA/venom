7c8386520882577ffc23080f01a4c204b5c33a6b
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-13 16:55:11 +0530

    Create settings.py

---
+++ plugins/Broadcast/settings.py
@@ -0,0 +1,6 @@
+#@crazybot filter bot v2
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.errors import ButtonDataInvalid, FloodWait
+
+
