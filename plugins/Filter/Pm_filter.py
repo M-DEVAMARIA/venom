8446f2d1859e85c2a6a6ec1c0c0745d1e3a2198d
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-17 00:20:54 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -841,7 +841,7 @@ async def group(client, message):
                         ),
                    ]]
         if not files: 
-             await advantage_spell_chok(msg)
+             await advantage_spell_chok(message)
              return
            # spf = await message.reply_text(
          #   text=f"<code>Sorry, I didn't get any files matches with your keyword, maybe your spelling is wrong. try sending the proper movie name...</code>",
