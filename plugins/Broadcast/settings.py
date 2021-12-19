f93f33d1e36977890f41ebc8826d19c619e4a204
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-19 19:18:04 +0530

    Update settings.py

--- plugins/Broadcast/settings.py
+++ plugins/Broadcast/settings.py
@@ -51,7 +51,7 @@ async def bot_info(bot, update: CallbackQuery):
     reply_markup = InlineKeyboardMarkup(buttons)
     await update.message.edit_text( 
         reply_markup=reply_markup,
-        text= f"coustime your {update.message.chat.title}settings:-\n\n button - {cap}\nIMDB - {imd}\nspelling - {spellc}\nautofilter- {autoc}",
+        text= f"**coustime your** {update.message.chat.title} **settings**\n\nCurrent settings B\n\n⪼Button: {cap}\n\n⪼Imdb: {imd}\n\n⪼Spelling mode: {spellc}\n\n⪼AutoFilter- {autoc}",
         parse_mode="html")
     
 @Client.on_callback_query(filters.regex(r"inPM\((.+)\)"), group=2)
