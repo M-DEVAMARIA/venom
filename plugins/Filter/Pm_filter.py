c3e7825e5624fe2eb71fab7a2b6ae910ce8e20ee
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-20 19:33:49 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -834,7 +834,7 @@ async def group(client, message, spoll=False):
             nyva=botusername.username
             BOT["username"]=nyva
         files = await get_filter_results(query=search)
-        if not pm_file_chat:
+        if not configs :
             await message.reply_text(text= "error occurred")
     if autoftr:
         if files:
