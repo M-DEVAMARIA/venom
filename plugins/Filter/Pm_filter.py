6f34efe4d8bfa92dc7f3c2605155de9ce1fecbce
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-21 15:37:13 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -919,7 +919,7 @@ async def group(client, message, spoll=False):
 async def advantage_spell_chek(msg):
   #  query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|send|snd|movie(s)?|new|latest|br((o|u)h?)*|^h(e)?(l)*(o)*|mal(ayalam)?|tamil|file|that|give|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle)", "", msg.text) # plis contribute some common words 
     query = re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", msg.text)
-    query = query.strip()
+ #   query = query.strip()
     gs_parsed = []
     if not query:
         k = await msg.reply("No valid movie name given")
