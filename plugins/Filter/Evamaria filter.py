854b68e4bb6897396be823ce3212e6bc60914296
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-22 15:56:13 +0530

    Update Evamaria filter.py

--- plugins/Filter/Evamaria filter.py
+++ plugins/Filter/Evamaria filter.py
@@ -33,7 +33,7 @@ async def advantage_spoll_choker(bot, query):
         buttons = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{file.file_size} {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
