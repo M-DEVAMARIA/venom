be82761c353c9be041067332d85c4f25151fb5af
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-24 23:51:13 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -242,7 +242,7 @@ async def advantage_spoll_choker(bot, query):
         buttons = [
             [
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)} {file.file_name",url=f"https://t.me/{temp.U_NAME}?start=subinps_-_-_-_{file.file_id}"
+                    text=f"{get_size(file.file_size)} {file.file_name}", url=f"https://t.me/{temp.U_NAME}?start=subinps_-_-_-_{file.file_id}"
                 ),
             ]
             for file in files
