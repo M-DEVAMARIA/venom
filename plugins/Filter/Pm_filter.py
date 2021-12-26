1edc47666caa136a52e280005f2cb4f3a130406c
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-26 15:34:51 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -267,7 +267,10 @@ async def advantage_spoll_choker(bot, query):
             buttons.append(
                 [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages")]
             )
-        await message.reply_text("check", reply_markup=InlineKeyboardMarkup(buttons))
+        imdb = await get_poster(b)
+        if imdb:
+           cap = IMDB_TEMPLATE.format(title = imdb['title'], url = imdb['url'], year = imdb['year'], genres = imdb['genres'], plot = imdb['plot'], rating = imdb['rating'], languages = imdb["languages"], runtime = imdb["runtime"], countries = imdb["countries"], release_date = imdb['release_date'],**locals())
+           await message.reply_photo(photo=poster.get("poster"),caption= cap, reply_markup=InlineKeyboardMarkup(buttons))
 
     
 @Client.on_callback_query()
