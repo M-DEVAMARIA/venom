955b96d2562ec1e91dd8b28c770c2706c89e7cbf
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-27 11:43:43 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -388,7 +388,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
 
         elif query.data.startswith("subinps"):
-            ident, file_id = query.data.split("#")
+            ident, file_id,user = query.data.split("#")
             filedetails = await get_file_details(file_id)
             for files in filedetails:
                 title = files.file_name
