267faff70b9862fb4f20d916a0da2a5ad30a8370
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-12-29 15:56:02 +0530

    Update settings.py

--- plugins/Broadcast/settings.py
+++ plugins/Broadcast/settings.py
@@ -15,7 +15,7 @@ from info import ADMINS
 async def botsetting_info(client, message):
     chat_id = message.chat.id
     userid = message.from_user.id
-    st = await client.get_chat_member(grp_id, userid)
+    st = await client.get_chat_member(chat_id, userid)
     if not (st.status == "creator") or (str(userid) in ADMINS):
         return
     buttons = [[
