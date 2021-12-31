3478b00deb04c0013dece27bff2dc42b37ce105b
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-01 00:53:39 +0530

    Update Commands.py

--- plugins/Filter/Commands.py
+++ plugins/Filter/Commands.py
@@ -52,7 +52,7 @@ async def gstart(bot, cmd):
         await db.add_user(cmd.from_user.id, cmd.from_user.first_name)
         await bot.send_message(
             LOG_CHANNEL,
-            f"#NEWUSER: \n\Name - [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})\nID - {cmd.from_user.id}")
+            f"#NEWUSER: \nName - [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})\nID - {cmd.from_user.id}")
          
     usr_cmdall1 = cmd.text
     if usr_cmdall1.startswith("/start subinps"):
