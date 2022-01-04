3d48298aa525f06f6c4bfaa4d9f32557d8dedacc
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-04 13:51:08 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -895,12 +895,11 @@ async def group(client, message):
                   user = message.from_user.id 
                   search = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
                   
-                  movies = await get_poster(search, bulk=True)
-                  movie = []
-                  if movies:
-                      for i in movie: 
-                         movies += i.strip() + movies['year']
-                        
+                  imdb = await get_poster(search, bulk=True)
+                  movies = []
+                  if imdb:
+                      for movie in imdb: 
+                        movies += movie.get('title')
                   movies = list(dict.fromkeys(movies))
                   if not movies:
                         return await message.reply_text(f"i couldn't find anything with {search}")
