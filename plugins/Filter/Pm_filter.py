7a0b6175f104dde40a7aeb0478b060cc22852082
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-04 14:38:08 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -896,16 +896,14 @@ async def group(client, message):
                   search = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
                   
                   imdb = await get_poster(search, bulk=True)
-                  imdb = list(dict.fromkeys(imdb))
-                  movies = imdb
                   if not movies:
-                        return await message.reply_text(f"i couldn't find anything with {search}")
+                       return await message.reply_text(f"i couldn't find anything with {search}")
                   if advance: 
                     btn = [
                        [
                            InlineKeyboardButton(
                            text=f"{movie.get('title')}",
-                           callback_data=f"spolling#{user}#{single}#{imdbg}#{max_pages}#{delete}#{delete_time}#{movie}",
+                           callback_data=f"spolling#{user}#{single}#{imdbg}#{max_pages}#{delete}#{delete_time}#{movie.movieID}",
                            )
                         ]
                         for movie in movies
