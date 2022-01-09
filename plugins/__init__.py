56c288a1f630bd2bd4d23005f694b022ecbfaaa8
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-09 23:32:33 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -23,30 +23,30 @@ START_BTN = InlineKeyboardMarkup(
 CALCULATE_TEXT = "Made by @FayasNoushad"
 CALCULATE_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton("DEL", callback_data="DEL"),
-        InlineKeyboardButton("AC", callback_data="AC"),
-        InlineKeyboardButton("(", callback_data="("),
-        InlineKeyboardButton(")", callback_data=")")
+        InlineKeyboardButton("DEL", callback_data="cal#DEL"),
+        InlineKeyboardButton("AC", callback_data="cal#AC"),
+        InlineKeyboardButton("(", callback_data="cal#("),
+        InlineKeyboardButton(")", callback_data="cal#)")
         ],[
-        InlineKeyboardButton("7", callback_data="7"),
-        InlineKeyboardButton("8", callback_data="8"),
-        InlineKeyboardButton("9", callback_data="9"),
-        InlineKeyboardButton("÷", callback_data="/")
+        InlineKeyboardButton("7", callback_data="cal#7"),
+        InlineKeyboardButton("8", callback_data="cal#8"),
+        InlineKeyboardButton("9", callback_data="cal#9"),
+        InlineKeyboardButton("÷", callback_data="cal#/")
         ],[
-        InlineKeyboardButton("4", callback_data="4"),
-        InlineKeyboardButton("5", callback_data="5"),
-        InlineKeyboardButton("6", callback_data="6"),
-        InlineKeyboardButton("×", callback_data="*")
+        InlineKeyboardButton("4", callback_data="cal#4"),
+        InlineKeyboardButton("5", callback_data="cal#5"),
+        InlineKeyboardButton("6", callback_data="cal#6"),
+        InlineKeyboardButton("×", callback_data="cal#*")
         ],[
-        InlineKeyboardButton("1", callback_data="1"),
-        InlineKeyboardButton("2", callback_data="2"),
-        InlineKeyboardButton("3", callback_data="3"),
-        InlineKeyboardButton("-", callback_data="-"),
+        InlineKeyboardButton("1", callback_data="cal#1"),
+        InlineKeyboardButton("2", callback_data="cal#2"),
+        InlineKeyboardButton("3", callback_data="cal#3"),
+        InlineKeyboardButton("-", callback_data="cal#-")
         ],[
-        InlineKeyboardButton(".", callback_data="."),
-        InlineKeyboardButton("0", callback_data="0"),
-        InlineKeyboardButton("=", callback_data="="),
-        InlineKeyboardButton("+", callback_data="+"),
+        InlineKeyboardButton(".", callback_data="cal#."),
+        InlineKeyboardButton("0", callback_data="cal#0"),
+        InlineKeyboardButton("=", callback_data="cal#="),
+        InlineKeyboardButton("+", callback_data="cal#+")
         ]]
     )
 
