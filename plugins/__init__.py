6d662e22b30c0b05b9d43398696fe360a008aad3
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-10 22:37:27 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -63,7 +63,7 @@ HELP = InlineKeyboardMarkup(
         InlineKeyboardButton('Fɪʟᴇ sᴛᴏʀᴇ', callback_data='batch')
         ],[
         InlineKeyboardButton('settings', callback_data='sett'),
-        InlineKeyboardButton('ᴄᴀʟᴄᴜʟᴀᴛᴏʀ', callback_data="calculater"),
+        InlineKeyboardButton('ᴄᴀʟᴄᴜʟᴀᴛᴏʀ', callback_data="clcltr"),
         InlineKeyboardButton('ᴄᴏᴠɪᴅ', callback_data='covid')
         ],[
         InlineKeyboardButton('ᴄᴏᴜɴᴛʀʏ', callback_data='cal'),
