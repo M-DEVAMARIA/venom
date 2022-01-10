b8f72ef9b4a66c27629b9d423abbfea693573701
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-10 10:41:09 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -915,7 +915,7 @@ async def group(client, message):
             data = BUTTONS[keyword]
             buttons = data['buttons'][0].copy()  
             buttons.append(
-            [InlineKeyboardButton(text="All", callback_data=f"spcheck#{files}#{message.from_user.id})]
+            [InlineKeyboardButton(text="All", callback_data=f"spcheck#{files}#{message.from_user.id}")]
             )    
             buttons.append(
             [InlineKeyboardButton(text="Next Page ⏩", callback_data=f"next_0_{keyword}")]
