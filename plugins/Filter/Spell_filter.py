5435381470c858b63ca88a2d848b1d63ee800f62
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-14 21:07:39 +0530

    Update Spell_filter.py

--- plugins/Filter/Spell_filter.py
+++ plugins/Filter/Spell_filter.py
@@ -18,7 +18,7 @@ async def advancespellmode(message, single, imdbg, max_pages, delete, delete_tim
     search = message.text
     user = message.from_user.id 
     search = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
-    search = search.strip() + "movie"
+    search = search.strip()
   #  search = await search_gagala(search)
 #    search+= await search_gagala(message.text)
   #   
