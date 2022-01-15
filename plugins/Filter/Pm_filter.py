45d5f07f9e868afee109079e942201ce39940ba2
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-15 20:20:08 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -841,7 +841,8 @@ async def group(client, message, spell=False):
             if not nyva:
                 botusername=await client.get_me()
                 nyva=botusername.username
-                BOT["username"]=nyva
+                BOT["username"]=nyva 
+                searchs=search
               #  searchs = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
             files = await get_filter_results(query=search)
             print("testing autof")
