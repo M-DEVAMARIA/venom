53256c98d500e3cdf4b5da7c9086273fef5fb86a
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-17 00:52:30 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -410,7 +410,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
         if query.data.startswith("checksub"):
             if AUTH_CHANNEL and not await is_subscribed(client, query):
-                await query.answer("I Like Your Smartness, But Don't Be Oversmart 😒",show_alert=True)
+                await query.answer("🛑 Join updates channel and press refresh button To get movie",show_alert=True)
                 return
             ident, file_id = query.data.split("#")
             filedetails = await get_file_details(file_id)
