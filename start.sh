e14890d97b4cfa0176c4739afa636b5bac4283b1
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-20 21:46:56 +0530

    Update and rename Procfile.sh to start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/M-DEVAMARIA/venom.git /venom
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /venom 
 fi
-cd /EvaMaria
+cd /venom
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting venom Bot ...."
 python3 bot.py
