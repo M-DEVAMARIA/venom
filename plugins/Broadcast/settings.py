e1da9558f5b15416641f1bf5ed2b9851e912e0f8
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-24 10:26:51 +0530

    Update settings.py

--- plugins/Broadcast/settings.py
+++ plugins/Broadcast/settings.py
@@ -360,7 +360,8 @@ async def custm_spell(bot, update: CallbackQuery):
     
     spell = await bot.ask(chat_id=chat,text="please send a custom message to set spell check message\nexample:-\nhey,{name},i cant find movie with your search {search}")
     k = spell
-    TEMPLATE[chat]=k
+    TEMPLATE[chat]=k 
+    print(f"{k}")
     buttons =[[InlineKeyboardButton("ON ✅", callback_data=f"set(spell_template|k|{chat}|{value})")]]        
     reply_markup=InlineKeyboardMarkup(buttons) 
     await spell.reply_text("confirm to set this is your spell check message",reply_markup=reply_markup)
