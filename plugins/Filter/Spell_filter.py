e9bee5c574f39f01a500328bc3e1eeb833386e61
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-30 22:53:37 +0530

    Update Spell_filter.py

--- plugins/Filter/Spell_filter.py
+++ plugins/Filter/Spell_filter.py
@@ -65,7 +65,7 @@ async def custombutton(msg):
     if not buttons:
         return False 
     name, url = buttons.split('-')
-    for - in buttons:
+    for '-' in buttons:
        if len(-) > 1 :
            reply_markup=InlineKeyboardMarkup(
             [[  
