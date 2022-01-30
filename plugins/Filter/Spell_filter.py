ba98b602273c0298ff8e3adec6f74a1378878732
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-30 20:06:52 +0530

    Mmmm

--- plugins/Filter/Spell_filter.py
+++ plugins/Filter/Spell_filter.py
@@ -60,8 +60,8 @@ async def custombutton(msg):
     buttons = "venom - https://t.me/venom_moviebot"
     if not buttons:
         return False 
-    name, url = buttons.split('-'):
-    if len('-' in buttons) > 1
+    name, url = buttons.split('-')
+    if len('-' in buttons) > 1 :
          reply_markup=InlineKeyboardMarkup(
             [[  
              InlineKeyboardButton(f"{name}", url= url)
