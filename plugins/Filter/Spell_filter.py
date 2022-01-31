0d2315b85c62899a7ef2e5a281a6fe15ecfc69fc
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-01-31 19:49:28 +0530

    Update Spell_filter.py

--- plugins/Filter/Spell_filter.py
+++ plugins/Filter/Spell_filter.py
@@ -56,8 +56,8 @@ async def normalspellmode(message, template):
     return
    
 async def custombutton(msg):
-    db = await db.find_chat(msg.chat.id)
-    buttons = db["settings"]["custom_button"]
+    let = await db.find_chat(msg.chat.id)
+    buttons = let["settings"]["custom_button"]
   #  buttons = """venom - https://t.me/venom_moviebot|venom - https://t.me/venom_moviebot\ndrishyam - https://t.me/mdadmin2|drishyam - https://t.me/mdadmin2"""     
     if not '\n' in buttons:
         if not '|' in buttons:
