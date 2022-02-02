62683d9421b63c00700ca53e1632dbd5844526cb
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-02 14:55:50 +0530

    Fix

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -768,7 +768,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         i, movie, year = query.data.split('#')
         await query.message.delete()
         await client.send_message(LOG_CHANNEL,
-                                 f'#request\nFrom - {query.message.user_mention}\n\n<b>movie info:</b>\nName: {movie}\nYear: {year}')
+                                 f'#request\nFrom - {query.message.from_user.mention}\n\n<b>movie info:</b>\nName: {movie}\nYear: {year}')
                                                                                                                          
     elif query.data == "index":
         await index_files(client, query, query)
