69cb9218b997405d8ee46eb33f892d8051fa91b7
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-02 17:24:27 +0530

    Update settings.py

--- plugins/Broadcast/settings.py
+++ plugins/Broadcast/settings.py
@@ -18,10 +18,10 @@ IMDBTEMPLATE ={}
 @Client.on_message(filters.command("setbutton"))
 async def buttonmode(bot, msg):
      args = msg.text.html.split(None, 1)
-     extracted = split_quotes(args[1])
+    # extracted = split_quotes(args[1])
      if not args:
          return await msg.err('nothing found')
-     reply_text, btn = parse_buttons(extracted[1])
+     reply_text, btn = parse_buttons(args[1])
      return await msg.reply_text(f"reply: {reply_text}\nbtn: {btn}")
   
 @Client.on_message(filters.command(['settings']))
