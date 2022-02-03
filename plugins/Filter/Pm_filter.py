a9a22fc72b5d6df0017c0f41537bc2c2695bf9f0
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-03 14:07:54 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -767,17 +767,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
          i, use = query.data.split('#')
          status = await db.get_mode(query.from_user.id)
          if use=='update':
-             if status['mode']== [True, 'True']:
-                value = 'False'
+             if status['mode']=='True' or True:
+                value = 'False'# or False 
              else:
-                value = 'True'
+                value = 'True' #or True
              default = {'pm_filter': True, 'mode': value}
              await db.update_mode(query.from_user.id, default)
              status = await db.get_mode(query.from_user.id)
-             reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('ADVANCE ✅' if status['mode'] else 'ADVANCE', callback_data='mode#update'), InlineKeyboardButton('NORMAL' if status['mode'] else 'NORMAL ✅', callback_data='mode_#update')],[InlineKeyboardButton('back', callback_data="start")]])
+             reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('ADVANCE ✅' if status['mode']=='True' else 'ADVANCE', callback_data='mode#update'), InlineKeyboardButton('NORMAL' if status['mode']=='True' else 'NORMAL ✅', callback_data='mode_#update')],[InlineKeyboardButton('back', callback_data="start")]])
              return await query.message.edit_reply_markup(reply_markup)
          else:
-             return await query.message.edit_text(text='you can choose bot features advance or normal as your wish', reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('ADVANCE ✅' if status['mode'] else 'ADVANCE', callback_data='mode#update'), InlineKeyboardButton('NORMAL' if status['mode'] else 'NORMAL ✅', callback_data='mode#update')]]))
+             return await query.message.edit_text(text='you can choose bot features advance or normal as your wish', reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('ADVANCE ✅' if status['mode']=='True' else 'ADVANCE', callback_data='mode#update'), InlineKeyboardButton('NORMAL' if status['mode']=='True' else 'NORMAL ✅', callback_data='mode#update')],[InlineKeyboardButton('back', callback_data="start")]]))
          
     elif query.data.startswith("request"):
         await query.answer('your Request successful', show_alert=True)
