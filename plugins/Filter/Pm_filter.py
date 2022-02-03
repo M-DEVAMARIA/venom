2bbb59efa610c160349e699c84c06ace9ce06c73
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-03 11:37:26 +0530

    Update Pm_filter.py

--- plugins/Filter/Pm_filter.py
+++ plugins/Filter/Pm_filter.py
@@ -768,16 +768,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
          status = await db.get_mode(query.from_user.id)
          if use=='update':
              if status['mode']=='True':
-                value = 'False'
+                value = False
              else:
-                value = 'True'
+                value = True
              default = {'pm_filter': True, 'mode': value}
              await db.update_mode(query.from_user.id, default)
              status = await db.get_mode(query.from_user.id)
              reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('ADVANCE ✅' if status['mode'] else 'ADVANCE', callback_data='mode#update'), InlineKeyboardButton('NORMAL' if status['mode'] else 'NORMAL ✅', callback_data='mode_#update')]])
              return await query.message.edit_reply_markup(reply_markup)
          else:
-             return await query.message.edit_text(text='you can choose bot features advance or normal as your wish', reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('ADVANCE ✅' if status else 'ADVANCE', callback_data='mode#update'), InlineKeyboardButton('NORMAL' if status else 'NORMAL ✅', callback_data='mode#update')]]))
+             return await query.message.edit_text(text='you can choose bot features advance or normal as your wish', reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('ADVANCE ✅' if status['mode'] else 'ADVANCE', callback_data='mode#update'), InlineKeyboardButton('NORMAL' if status['mode'] else 'NORMAL ✅', callback_data='mode#update')]]))
          
     elif query.data.startswith("request"):
         await query.answer('your Request successful', show_alert=True)
