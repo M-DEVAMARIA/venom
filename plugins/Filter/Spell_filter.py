da06f94c420b6e937170e2d5f14a2068ebd76533
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-03 23:33:36 +0530

    Update Spell_filter.py

--- plugins/Filter/Spell_filter.py
+++ plugins/Filter/Spell_filter.py
@@ -56,7 +56,7 @@ async def normalspellmode(message, template):
 async def get_button(msg):
    info = await db.find_chat(msg.chat.id)
    button = info["configs"]["custom_button"]
-   i, button = parse_buttons(button)
+   i, button = parse_buttons(str(button))
    return button
 
 def parse_buttons(text):
