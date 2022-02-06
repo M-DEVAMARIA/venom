6b7f2eb2119755dfd39fcff7f5cac57d50b1eae4
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-02-06 16:07:29 +0530

    Fix

--- plugins/Filter/Spell_filter.py
+++ plugins/Filter/Spell_filter.py
@@ -44,10 +44,10 @@ async def normalspellmode(message, template):
     search = message.text
     info = await db.find_chat(message.chat.id)
     button = info["configs"]["custom_button"]
-    buttons = parse_buttons(str(button))
-    reply_button = None#InlineKeyboardMarkup(buttons) if buttons!='[]' else InlineKeyboardMarkup([[InlineKeyboardButton("🔍 GOOGLE ", url=f'https://www.google.com/search?q={search}'), InlineKeyboardButton("IMDB 🔎", url=f'https://www.imdb.com/search?q={search}')]])
+    buttons = parse_buttons(button)
+    reply_button = InlineKeyboardMarkup(buttons) if button !='None' else InlineKeyboardMarkup([[InlineKeyboardButton("🔍 GOOGLE ", url=f'https://www.google.com/search?q={search}'), InlineKeyboardButton("IMDB 🔎", url=f'https://www.imdb.com/search?q={search}')]])
     spf = await message.reply_text(
-    text=f"<code>Sorry {button} {message.from_user.mention},\n\n<b>I didn't get any files matches with {search}, maybe your spelling is wrong. try sending the proper movie name...</b></code>" if template=="None" else template.format(name=message.from_user.mention, search=search),
+    text=f"<code>Sorry {message.from_user.mention},\n\n<b>I didn't get any files matches with {search}, maybe your spelling is wrong. try sending the proper movie name...</b></code>" if template=="None" else template.format(name=message.from_user.mention, search=search),
     reply_markup=reply_button,
     parse_mode="html",
     reply_to_message_id=message.message_id)
