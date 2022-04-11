43f3701aa07c1594ec136017efa054d8e73727a7
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-11 16:31:21 +0530

    Fix

--- plugins/Filter/Spell_filter.py
+++ plugins/Filter/Spell_filter.py
@@ -21,8 +21,8 @@ async def advancespellmode(msg):
     query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", search, flags=re.IGNORECASE)
     query = query.strip() + " movie"
     dump = DUPLICATES.get(query)
-    if dump and dump.get("movielist"):
-       movielist = dump.get("movielist")
+    if dump:
+       movielist = dump
        await msg.reply_text("using already stored")
     else:
        g_s = await search_gagala(query)
